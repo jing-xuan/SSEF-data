bf3f3701e9f82622550c037624c7fa44f97b7565
jinx73 <tayjxn@gmail.com>
2018-03-01 22:30:14 +0800

    display v1

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ function readContent(callback) {
 }
 function parseProject(jsonObj){
   //60 - finals, 20 - file submitted, 10 - registered, 5 - saved
+  var complete = [];
   var all = [
     {
       school: "NUSH",
@@ -31,6 +32,14 @@ function parseProject(jsonObj){
         cs: 0
       },
       registered: {
+        total: 0,
+        bi: 0,
+        cm: 0,
+        ph: 0,
+        ma: 0,
+        cs: 0
+      },
+      saved: {
         total: 0,
         bi: 0,
         cm: 0,
@@ -65,6 +74,14 @@ function parseProject(jsonObj){
         ph: 0,
         ma: 0,
         cs: 0
+      },
+      saved: {
+        total: 0,
+        bi: 0,
+        cm: 0,
+        ph: 0,
+        ma: 0,
+        cs: 0
       }
     },
     {
@@ -87,6 +104,14 @@ function parseProject(jsonObj){
         cs: 0
       },
       registered: {
+        total: 0,
+        bi: 0,
+        cm: 0,
+        ph: 0,
+        ma: 0,
+        cs: 0
+      },
+      saved: {
         total: 0,
         bi: 0,
         cm: 0,
@@ -121,44 +146,76 @@ function parseProject(jsonObj){
         ph: 0,
         ma: 0,
         cs: 0
+      },
+      saved: {
+        total: 0,
+        bi: 0,
+        cm: 0,
+        ph: 0,
+        ma: 0,
+        cs: 0
       }
     }
   ];
+  var timeline = [];
   for(var i = 0; i< Object.keys(jsonObj).length; i++){
     var schList = ["NUS HIGH SCHOOL OF MATHEMATICS AND SCIENCE", "RAFFLES INSTITUTION", "HWA CHONG INSTITUTION", "NATIONAL JUNIOR COLLEGE"];
-    var statusList = ["60", "20", "10"];
-    var status = ["final", "fileSubmitted", "registered"];
+    var statusList = ["60", "20", "10", "5"];
+    var status = ["final", "fileSubmitted", "registered", "saved"];
     var bi = ["AS", "BI", " BM", "BE", "CB", "CO", "EE", "MI", "PS", "TM", "EA"];
     var ph = ["ES", "EP", "MS", "PH", "EM"];
     var cm = ["CH", "EC"];
     var ma = ["MA"];
     var cs = ["RO", "SS"];
+    var proj = {
+      "ID" : "",
+      "sbj" : "",
+      "sch" : "",
+      "status" : "",
+      "subTime" : ""
+    };
     if(schList.indexOf(jsonObj[i]['school']) != -1){
       all[schList.indexOf(jsonObj[i]['school'])]['projects']++;
       if(statusList.indexOf(jsonObj[i]['status']) != -1 && jsonObj[i]['category1'] != 'JBO' && jsonObj[i]['category1'] != 'JCH' && jsonObj[i]['category1'] != 'JCO' && jsonObj[i]['category1'] != 'JMA' && jsonObj[i]['category1'] != 'JPH' ){
         all[schList.indexOf(jsonObj[i]['school'])][status[statusList.indexOf(jsonObj[i]['status'])]]['total']++;
+        proj["status"] = jsonObj[i]['status'];
+        proj["sch"] = jsonObj[i]['school'];
+        proj["ID"] = i;
+        proj["subTime"] = jsonObj[i]['submissionDate'];
         if(bi.indexOf(jsonObj[i]['category1']) != -1){
+          proj["sbj"] = "bi";
+          timeline.push(proj);
           all[schList.indexOf(jsonObj[i]['school'])][status[statusList.indexOf(jsonObj[i]['status'])]]['bi']++;
         }else if(cm.indexOf(jsonObj[i]['category1']) != -1){
+          proj["sbj"] = "cm";
+          timeline.push(proj);
           all[schList.indexOf(jsonObj[i]['school'])][status[statusList.indexOf(jsonObj[i]['status'])]]['cm']++;
         }else if(ph.indexOf(jsonObj[i]['category1']) != -1){
+          proj["sbj"] = "ph";
+          timeline.push(proj);
           all[schList.indexOf(jsonObj[i]['school'])][status[statusList.indexOf(jsonObj[i]['status'])]]['ph']++;
         }else if(ma.indexOf(jsonObj[i]['category1']) != -1){
+          proj["sbj"] = "ma";
+          timeline.push(proj);
           all[schList.indexOf(jsonObj[i]['school'])][status[statusList.indexOf(jsonObj[i]['status'])]]['ma']++;
         }else if(cs.indexOf(jsonObj[i]['category1']) != -1){
+          proj["sbj"] = "cs";
+          timeline.push(proj);
           all[schList.indexOf(jsonObj[i]['school'])][status[statusList.indexOf(jsonObj[i]['status'])]]['cs']++;
         }
       }
     }
   }
-  return all;
+  complete.push(all);
+  complete.push(timeline);
+  return complete;
 }
 
 app.get("/", function(req, res){
   readContent(function (err, content) {
       var jsonObj = JSON.parse(content);
       var list = parseProject(jsonObj);
-      res.render('index.ejs', {projects: list})
+      res.render('index.ejs', {projects: list[0], timeline: list[1]})
   })
 })
 
